cb56fbff9051875895472a8bdc99282b993fa379
OberdanSoldi <oberdan_soldi@outlook.com.br>
2024-08-05 07:47:52 -0300

    feat: Add typography settings to theme object
    Expanded the theme object to include typography settings for body, title, button, and subtitle text elements. Each typography setting now includes font size and line height properties, as well as appropriate font family designations.

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,3 +1,27 @@
 export const theme = {
-	backgroundColor: '#0c0c0d'
+	backgroundColor: '#0c0c0d',
+	typography: {
+		body: {
+			fontSize: '12px',
+			lineHeight: '130%'
+		},
+		title: {
+			fontSize: '24px',
+			lineHeight: '100%'
+		},
+		button: {
+			fontSize: '14px',
+			lineHeight: '130%'
+		},
+		subtitle: {
+			fontSize: '14px',
+			lineHeight: '130%'
+		},
+		fonts: {
+			title: 'Inter_700Bold',
+			body: 'Inter_400Regular',
+			subtitle: 'Inter_900Black',
+			button: 'Inter_600SemiBold'
+		}
+	}
 };
